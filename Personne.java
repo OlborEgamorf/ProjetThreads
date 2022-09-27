991388d55aaa9a1b28e477c4023bdb137c7ed154
AdrianG1134 <114444445+AdrianG1134@users.noreply.github.com>
2022-09-27 10:16:23 +0200

    Personne

---
+++ Personne.java
@@ -0,0 +1,28 @@
+package test;
+
+public class Personne extends Thread {
+    private double age;
+    private double vitesse;
+    private boolean = false;
+    //Math.random Math.floor
+    Personne(double age, double vitesse){
+        this.age= age;
+        this.vitesse= vitesse;
+    }
+
+    public void run (){
+
+    }
+
+    public static void baignade(){
+        if (x,y == plage)
+
+
+    }
+
+
+
+
+
+
+}
