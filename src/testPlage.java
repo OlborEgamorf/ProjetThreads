724c7ab56059eb379d46ec0b87d631bd6c4139d3
MagaliTendinite <114472492+MagaliTendinite@users.noreply.github.com>
2022-09-29 11:41:08 +0200

    Add files via upload

---
+++ src/testPlage.java
@@ -0,0 +1,20 @@
+package environnement;
+
+
+public class testPlage {
+    public static void main (String [] args){
+
+
+
+        environnement.plage p1 = new environnement.plage(40,20,10);
+
+        System.out.println(p1.coordToArray(3,2));
+
+
+
+
+
+
+    }
+
+}
