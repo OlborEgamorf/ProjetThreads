98da06dc122c5eef74b8e7e85bd7fb07a056dc4a
Olbor <32017465+OlborEgamorf@users.noreply.github.com>
2022-10-18 08:23:39 +0200

    Lags

--- src/Personne.java
+++ src/Personne.java
@@ -318,6 +318,12 @@ public class Personne extends Thread {
                 objPosition = positionPlage;
 
             } else if (etat == Etat.REPOS) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
             } else if (etat == Etat.PLACEMENT) {
 
