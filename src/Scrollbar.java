2bb86c8b19dc1af151a7754d21031a0606b6082f
MagaliTendinite <114472492+MagaliTendinite@users.noreply.github.com>
2022-10-19 20:25:06 +0200

    Create Scrollbar.java

---
+++ src/Scrollbar.java
@@ -0,0 +1,39 @@
+package src;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class test2 {
+    test2() {
+        Frame f = new Frame("Nombre de population");
+        final Label label = new Label();
+        label.setAlignment(Label.CENTER);
+        // Position texte
+        label.setSize(300, 150);
+        // Localisation fenetre
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - f.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - f.getHeight()) / 2);
+        f.setLocation(x, y);
+        // Parametre Scrollbar
+        final Scrollbar population = new Scrollbar(Adjustable.HORIZONTAL, 0, 0, 0, 2000);
+        // Position Scrollbar
+        population.setBounds(50, 0, 200, 50);
+        f.add(population);
+        f.add(label);
+        // Taille de la fenetre
+        f.setSize(300, 125);
+        f.setLayout(null);
+        f.setVisible(true);
+        population.addAdjustmentListener(new AdjustmentListener() {
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                label.setText("Nombre de personne : "+ population.getValue());
+            }
+        });
+    }
+
+    public static void main(String args[]){
+        new test2();
+    }
+}
